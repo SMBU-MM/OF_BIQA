a948467786cc7af66995479dd876bd9f54acad70
WANG Zhihua <1072020701@qq.com>
2023-04-18 00:35:06 +0800

    Update predictions.m

--- FR-IQAs/predictions.m
+++ FR-IQAs/predictions.m
@@ -3,6 +3,7 @@ fsimval = 0;
 sr_sim = 0;
 nlpds = 0;
 vsi_value = 0;
+gmsd_value = 0;
 count = 1;
 for i = 1:237200
     dist_name = strcat(num2str(i, '%08d'), '.png'); 
@@ -15,7 +16,8 @@ for i = 1:237200
     sr_sim(i,1) = SR_SIM(I2, I1);
     [nlpds(i,1),  ~] = NLP_dist(double(rgb2gray(I1))/255.0,double(rgb2gray(I2))/255.0);
     vsi_value(i,1) = VSI(I2, I1);
-    fprintf(fid,  "%s,%s,%f,%f,%f,%f\n", ref_name, dist_name, fsimval(i,1), sr_sim(i,1), nlpds(i,1), vsi_value(i,1));
+    [gmsd_value(i,1), ~] = GMSD(I1, I2)
+    fprintf(fid,  "%s,%s,%f,%f,%f,%f,%f\n", ref_name, dist_name, fsimval(i,1), sr_sim(i,1), nlpds(i,1), vsi_value(i,1), gmsd_value(i,1) );
     if mod(i,10)==0
         disp(['have completed:', num2str(i)])
     end
